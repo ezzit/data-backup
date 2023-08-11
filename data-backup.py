a8c85918aff1ccdfd23dba011fb213664a4931bb
ezzit <elzo.sigueta@proton.me>
2023-08-11 15:57:55 -0300

    stop sql server service and start again

--- data-backup.py
+++ data-backup.py
@@ -1,4 +1,14 @@
 import shutil
+import win32serviceutil
+
+service_name = 'MSSQL$SQLEXPRESS'
+
+# parar o serviço sql server (SQLEXPRESS)
+try:
+    win32serviceutil.StopService(service_name)
+    print(f'Serviço SQL Server ({service_name}) parado com sucesso!')
+except Exception as e:
+    print(f'Ocorreu um erro ao interromper o serviço SQL Server ({service_name}): {str(e)}')
 
 # caminho dos arquivos .mdf e .ldf
 mdf_file = 'C:/Program Files/Microsoft SQL Server/MSSQL15.SQLEXPRESS/MSSQL/DATA/siacRestaurante_Data.mdf'
@@ -9,10 +19,17 @@ mdf_backup = 'C:/Backup'
 ldf_backup = 'C:/Backup'
 
 try:
-    # Fazer cópia dos arquivos .mdf e .ldf
+    # fazer cópia dos arquivos .mdf e .ldf
     shutil.copy(mdf_file, mdf_backup)
     shutil.copy(ldf_file, ldf_backup)
 
     print('Backup realizado com sucesso!')
 except Exception as e:
     print(f'Erro ao realizar o backup: {e}')
+
+# iniciar o serviço sql server (SQLEXPRESS)
+try:
+    win32serviceutil.StartService(service_name)
+    print(f'Serviço SQL Server ({service_name}) foi iniciado novamente com sucesso.')
+except Exception as e:
+    print(f'Ocorreu um erro ao iniciar o serviço SQL Server ({service_name}): {str(e)}')
